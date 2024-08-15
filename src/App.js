4ae0b33216f7783265b6b8e898b1c6d917cb72d9
krapivnitskiyD <krapivnitskiy@itstep.academy>
2024-08-15 12:17:36 +0300

    adding info about band, album and covers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function AlbumInfo(props) {
+    return (
+        <div>
+            <p>{props.album}</p>
+            <img src={props.cover} />
+        </div>
+    );
+}
+
+
+function BandInfo() {
+
+    const band = {
+        name: 'AC/DC',
+        genre: 'Rock',
+        members: ['Angus Young', 'Phil Rudd', 'Cliff Williams', 'Brian Johnson', 'Stevie Young'],
+        albums: ['Back in Black', 'Highway to Hell', 'The Razors Edge', 'High Voltage', 'Dirty Dids Done Dirt Cheap'],
+        covers: ['alb1.jpg', 'alb2.jpg', 'alb3.jpg', 'alb4.jpg', 'alb5.jpg']
+    };
+
+    return (
+        <div>
+            <h1>{band.name}</h1>
+            <p><strong>Genre: </strong>{band.genre}</p>
+            <p><strong>Members: </strong>{band.members.join(', ')}</p>
+            <h2>Top albums:</h2>
+            <AlbumInfo album={band.albums[0]} cover={band.covers[0]}></AlbumInfo>
+            <AlbumInfo album={band.albums[1]} cover={band.covers[1]}></AlbumInfo>
+            <AlbumInfo album={band.albums[2]} cover={band.covers[2]}></AlbumInfo>
+            <AlbumInfo album={band.albums[3]} cover={band.covers[3]}></AlbumInfo>
+            <AlbumInfo album={band.albums[4]} cover={band.covers[4]}></AlbumInfo>
+        </div>
+    );
+
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <BandInfo></BandInfo>
+        </div>
+    );
 }
 
 export default App;
